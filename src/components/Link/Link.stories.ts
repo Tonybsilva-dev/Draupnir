6c34fb0f8d6c72ff7729ee615bc28b40788e78e2
Antonio Silva <antoniobsilvaprof@hotmail.com>
2024-01-10 11:50:09 -0300

    feat: add link component

---
+++ src/components/Link/Link.stories.ts
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import Link, { LinkProps } from "./Link";
+
+const meta: Meta<LinkProps> = {
+  title: "components/Link",
+  // tags: ["autodocs"],
+  component: Link,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    children: {
+      type: "string",
+    },
+    href: {
+      type: "string",
+    },
+    disabled: {
+      type: "boolean",
+    },
+    className: {
+      type: "string",
+    },
+  },
+};
+
+export default meta;
+
+export const Primary: StoryObj<LinkProps> = {
+  args: {
+    children: "Link",
+    href: "/",
+  },
+};
+
+export const Disabled: StoryObj<LinkProps> = {
+  args: {
+    children: "Link",
+    href: "/",
+    disabled: true,
+  },
+};
+
+export const Dark: StoryObj<LinkProps> = {
+  args: {
+    children: "Link",
+    href: "/",
+    className: "theme-dark",
+  },
+};

e8de0c0b0d5844683719cf2c68dee59cdc7409e0
Antonio Silva <antoniobsilvaprof@hotmail.com>
2024-01-11 16:47:11 -0300

    feat: add class-variance-authority on typography

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,40 +1,41 @@
 import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-export type TypographyProps = {
-  variant?: "primary" | "secondary" | "tertiary";
-  size?: "xs" | "sm" | "md" | "xl";
+const textVariants = cva("text-gray-primary text-sm font-normal", {
+  variants: {
+    variant: {
+      primary: "text-gray-primary",
+      secondary: "text-gray-secondary",
+      tertiary: "text-gray-tertiary",
+    },
+    size: {
+      xs: "text-xs",
+      sm: "text-sm",
+      md: "text-md",
+      xl: "text-xl",
+    },
+  },
+
+  defaultVariants: {
+    variant: "primary",
+    size: "md",
+  },
+});
+
+export type TypographyProps = VariantProps<typeof textVariants> & {
   children: React.ReactNode;
   element?: keyof JSX.IntrinsicElements;
 } & JSX.IntrinsicElements["p"];
 
-const variantClassMap = {
-  variant: {
-    primary: "text-gray-primary",
-    secondary: "text-gray-secondary",
-    tertiary: "text-gray-tertiary",
-  },
-  size: {
-    xs: "text-xs",
-    sm: "text-sm",
-    md: "text-md",
-    xl: "text-xl",
-  },
-};
-
 const Typography = ({
-  variant = "primary",
-  size = "md",
   children,
   element = "p",
+  className,
   ...rest
 }: TypographyProps) => {
   const Element = element as any;
   return (
-    <Element
-      className={`text-gray-primary text-sm font-normal 
-  ${variantClassMap.variant[variant]} ${variantClassMap.size[size]}`}
-      {...rest}
-    >
+    <Element className={`${textVariants(rest)} ${className}`} {...rest}>
       {children}
     </Element>
   );

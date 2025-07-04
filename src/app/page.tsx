f696eb9814b404b3af0b810a9ff85217910ca7f8
Antonio Bernardino da Silva <contato@antoniobsilva.com.br>
2025-07-04 00:46:17 -0300

    fix: build and tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Switch from "@/components/Switch/Switch";
+import Switch from "@/components/molecules/Switch/Switch";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-3 p-24 bg-background">
       <h1>Hello, Draupnir</h1>
 
-      <Switch/>
+      <Switch />
     </div>
   );
 }
